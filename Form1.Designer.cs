f3244b81e1b1c15cb58e3cffe850d9cb7df633b2
WooAf <phaonixweb@gmail.com>
2018-05-18 00:14:01 -0400

    INIT COMMIT

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Rom2WAD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rom2WAD));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.channelName = new System.Windows.Forms.TextBox();
+            this.channelID = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WADFILE = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(303, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(303, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(303, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // channelName
+            // 
+            this.channelName.Location = new System.Drawing.Point(12, 208);
+            this.channelName.MaxLength = 20;
+            this.channelName.Name = "channelName";
+            this.channelName.Size = new System.Drawing.Size(210, 20);
+            this.channelName.TabIndex = 7;
+            this.channelName.Text = "OoT Randomized";
+            this.channelName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // channelID
+            // 
+            this.channelID.Location = new System.Drawing.Point(303, 177);
+            this.channelID.Name = "channelID";
+            this.channelID.Size = new System.Drawing.Size(122, 20);
+            this.channelID.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(303, 96);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(122, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(42, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Change WAD Title  (Optional)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // WADFILE
+            // 
+            this.WADFILE.Location = new System.Drawing.Point(12, 12);
+            this.WADFILE.Name = "WADFILE";
+            this.WADFILE.Size = new System.Drawing.Size(210, 34);
+            this.WADFILE.TabIndex = 14;
+            this.WADFILE.Text = "1) SELECT ORIGINAL WAD FILE";
+            this.WADFILE.UseVisualStyleBackColor = true;
+            this.WADFILE.Click += new System.EventHandler(this.WADFILE_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 34);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "2) SELECT COMPRESSED RANDOMIZED ROM (32 MB)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 34);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "3) SELECT GZINJECT.EXE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 34);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "4) GENERATE WAD";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(270, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 55);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "DEBUG";
+            // 
+            // Rom2WAD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 240);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.WADFILE);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.channelID);
+            this.Controls.Add(this.channelName);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(250, 279);
+            this.MinimumSize = new System.Drawing.Size(250, 279);
+            this.Name = "Rom2WAD";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rom2WAD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox channelName;
+        private System.Windows.Forms.TextBox channelID;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button WADFILE;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Rom2WAD : Form
+    {
+        public Rom2WAD()
+        {
+            InitializeComponent();
+        }
+
+        static void GenerateID(string command)
+        {
+            try
+            {
+                ProcessStartInfo ProcessInfo;
+
+                ProcessInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+                Process.Start(ProcessInfo);
+            }
+            catch
+            {
+                MessageBox.Show("An error has occured.");
+            } 
+        }
+
+        private void WADFILE_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Select Original WAD File";
+            theDialog.Filter = "WAD files|*.wad";
+            theDialog.InitialDirectory = @"C:\";
+
+
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            string fullPath = theDialog.FileName;
+                            string fileName = theDialog.SafeFileName;
+                            string path = fullPath.Replace(fileName, "") + fileName;
+                            textBox1.Text = path;
+
+                            WADFILE.Text = fileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Select Z64 File";
+            theDialog.Filter = "Z64 files|*.z64";
+            theDialog.InitialDirectory = @"C:\";
+
+
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            string fullPath = theDialog.FileName;
+                            string fileName = theDialog.SafeFileName;
+                            string path = fullPath.Replace(fileName, "");
+                            textBox4.Text = path + fileName;
+
+                            button1.Text = fileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Select GZInject.exe";
+            theDialog.Filter = "Execute File|*.exe";
+            theDialog.InitialDirectory = @"C:\";
+
+
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            string fullPath = theDialog.FileName;
+                            string fileName = theDialog.SafeFileName;
+                            string path = fullPath.Replace(fileName, "");
+                            textBox2.Text = path;
+                            textBox3.Text = fileName;
+
+                            button2.Text = fileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            /*
+            GENERATE CHANNELID
+            */
+            Random rnd = new Random();
+
+            string chars1 = "BCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string chars2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int num1 = rnd.Next(0, chars1.Length - 1);
+            int num2 = rnd.Next(0, chars2.Length - 1);
+
+            char ran1 = chars1[num1];
+            char ran2 = chars2[num2];
+
+            channelID.Text = "N" + ran1 + ran2 + "E";
+
+            //Output
+            string output = System.Environment.CurrentDirectory + "\\" + channelName.Text + ".wad";
+
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please select the original WAD file.");
+            }
+            else if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Please select your GZInject.exe file.");
+            }
+            else
+            {
+                //DELETE common-key.bin
+                if (File.Exists(textBox2.Text + "common-key.bin"))
+                {
+                    File.Delete(textBox2.Text + "common-key.bin");
+                }
+
+                string cmdrun = "cd " + textBox2.Text + "&&" + textBox3.Text + " -a genkey" + "&&" + "gzinject --cleanup -a inject -w \"" + textBox1.Text + "\" -i " + channelID.Text + " -t \"" + channelName.Text + "\" -o \"" + output + "\" --rom \"" + textBox4.Text + "\" --disable-controller-remappings";
+
+                //MessageBox.Show(cmdrun);
+                GenerateID(cmdrun);
+            }
+        }
+    }
+}
